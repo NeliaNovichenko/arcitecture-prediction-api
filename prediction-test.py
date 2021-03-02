b7d3da5ff3fcd0a6872073bcbe69ff8010ed4486
Nelia Novichenko <nelia.novichenko@ihsmarkit.com>
2021-03-03 00:48:43 +0200

    latest

--- prediction-test.py
+++ prediction-test.py
@@ -5,11 +5,11 @@ execution_path = os.getcwd()
 
 prediction = CustomImagePrediction()
 prediction.setModelTypeAsResNet50()
-prediction.setModelPath(os.path.join(execution_path, "model_ex-010_acc-0.733146.h5"))
-prediction.setJsonPath(os.path.join(execution_path, "model_class.json"))
-prediction.loadModel(num_objects=4)
+prediction.setModelPath(os.path.join(execution_path, "./dataset/models/try2_model_ex-006_acc-0.714195.h5"))
+prediction.setJsonPath(os.path.join(execution_path, "./dataset/json/model_class.json"))
+prediction.loadModel(num_objects=25)
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "697_800px-H._V._Shaw_Building%2C_Edmonton_%281%29.jpg"), result_count=4)
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "./dataset/train/queenAnne/144_800px-Isbister_School.jpg"), result_count=25)
 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction, " : " , eachProbability)
--- training.py
+++ training.py
@@ -11,7 +11,6 @@ from modelTrainer import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsResNet50()
-# model_trainer.setDataDirectory("dataset_test")
 model_trainer.setDataDirectory("dataset")
-model_trainer.trainModel(num_objects=25, num_experiments=25, enhance_data=False, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=25, num_experiments=50, enhance_data=True, batch_size=32, show_network_summary=True, continue_from_model='./dataset/models/try3_model_ex-008_acc-0.809675.h5')
   
